e8c43540166f2963f7fee945ad40bc85c50ab29d
Prerna <iprernasingh4@gmail.com>
2023-09-28 14:07:23 +0530

    first commit

--- script.js
+++ script.js
@@ -46,3 +46,16 @@ function loadinganimation() {
   });
 }
 loadinganimation();
+
+document.addEventListener("mousemove", function (dets) {
+  gsap.to("#cursor", {
+    left: dets.x,
+    top: dets.y,
+  });
+});
+
+document.querySelector("child1").addEventListener("mouseenter", function () {
+  gsap.to("#cursor", {
+    transform: "translate(-50%, -50%) scale(0)",
+  });
+});
