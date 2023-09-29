019e77b814c11b163db44030f71b67a95f9e77cb
Prerna <iprernasingh4@gmail.com>
2023-09-29 20:36:43 +0530

    first commit

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ videoconAnimation();
 function loadinganimation() {
   gsap.from("#page1 h1", {
     y: 100,
-    opacity: 0
+    opacity: 0,
     delay: 0.3,
     duration: 0.9,
     stagger: 0.3,
